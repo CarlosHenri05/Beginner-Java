a8e304d1ff4b0d86b1746267ee01ed99041d2b25
Carlos <carloshenrique20051bb@gmail.com>
2024-08-17 02:36:06 -0300

    Add files via upload

---
+++ Projetos-Iniciante/Conversor-de-moedas/src/conversor/Moedas.java
@@ -0,0 +1,18 @@
+package conversor;
+
+public class Moedas {
+
+   public double real, dolar, euro, iene;
+
+   public Moedas() {
+      // Cada valor das moedas já está convertido em real, 1 dolar = 5.46 reais, por exemplo.
+      this.real = 1;
+      this.dolar = 5.46;
+      this.euro = 6.03;
+      this.iene = 0.037;
+   }
+}
+
+
+
+
